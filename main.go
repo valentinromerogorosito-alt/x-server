46ba4e9fe115ebe4b9b0cdc0efbee038a2704649
valentinromerogorosito-alt <valentinromerogorosito@gmail.com>
2026-06-27 22:44:26 -0300

    expanded the implementation

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-    "fmt"
+    "log"
     "net/http"
 )
 
+func handlerFunc(w ResponseWriter, r *Request) {
+	header := "Content-Type: text/plain; chaset=utf-8"
+	w.WriteHeader(header)
+	w.Write("OK")
+}
+
 func main() {
+	const filepathRoot = "."
+    const port = "8080"
+
     mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", handlerFunc(http.ResponseWriter, *http.Request))
 
     srv := &http.Server{
-        Addr : ":8080",
+        Addr :   ":" + port,
         Handler: mux,
-    }
+	}
 
+    log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
     err := srv.ListenAndServe()
     if err == nil {
-        fmt.Println("Aborting")
+        log.Fatal(err)
     }
 }
 
